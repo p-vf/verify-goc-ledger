a7e8ec68306cb22b7246ea793cf4a5330cb83b59
pvonf <pvonf@shoreham>
2026-04-30 12:31:57 +0200

    add invalid generator

--- code/src/verify_goc_ledger/generate_repo/main.py
+++ code/src/verify_goc_ledger/generate_repo/main.py
@@ -76,6 +76,80 @@ class ValidRepoGeneratorV1:
 
 # TODO for v2: generate transactions where the number of transactions per person follows a pareto distribution
 
+class InvalidRepoGeneratorGoc:
+    def __init__(self, repo_dir: Path, num_commits: int, num_users: int, seed: str="hello", sign: bool = True):
+        self._repo_dir = repo_dir
+        self._num_commits = num_commits
+        self._num_users = num_users
+        self._sign = sign
+        self._seed = seed
+        pass
+
+    def generate(self):
+        if os.path.exists(self._repo_dir):
+            conf = input(f"the directory {self._repo_dir} exists already. Replace it? [Y/n] ")
+            if conf.lower() == "y":
+                print(f"deleting directory {self._repo_dir}")
+                shutil.rmtree(self._repo_dir)
+            else:
+                print("aborting")
+                exit(1)
+        
+        keydir = self._repo_dir/"keys"
+        repo = Repo(str(self._repo_dir), keydir.absolute() if self._sign else None)
+        repo.create_repo()
+        random.seed(self._seed)
+
+        authors = generate_human_names(self._num_users)
+
+        if self._sign:
+            authorkeys = get_public_keys(keydir, authors)
+            configure_allowed_signers(self._repo_dir, keydir, authors)
+            print("created or read authors:")
+            print("authors:", authorkeys, "\ncorresponding names:", authors)
+
+        ledger: dict[bytes, Account] = dict()
+
+        for a in authors:
+            ledger[a.encode()] = Account(a.encode())
+
+        for account in ledger.values():
+            act = account.create(1000)
+            add_as_commit(act, repo, msg="creation of tokens")
+        
+        invalid_commit_number = self._num_commits // 2
+
+        for i in range(self._num_commits):
+            l = list(ledger.values())
+            giver = random.choice(l)
+            l.remove(giver)
+            acker = random.choice(l)
+            amount = int(random.random() * giver.balance() * 0.2)
+            if i == invalid_commit_number:
+                old_amount = amount
+                amount = int(giver.balance() + 1)
+                give_act = giver.give(amount, acker.id)
+                giver.given[acker.id] = old_amount
+                print(f"fraudulent participant: {giver.id.decode()}")
+            else:
+                give_act = giver.give(amount, acker.id)
+            # By acknowledging the given amount (even when more than balance), the acker makes himself invalid as well
+            ack_act = acker.ack(amount, giver.id)
+            give_msg = f"{(giver.id.decode())} gave {amount} CHF to {(acker.id.decode())}, has given {giver.given[acker.id]} CHF"
+            ack_msg = f"{(acker.id.decode())} acked {amount} CHF from {(giver.id.decode())}, has acked {acker.acked[giver.id]} CHF"
+            commit_give = add_as_commit(give_act, repo, msg=give_msg, deps=repo.show_ref("refs/heads/frontier/CHF/" + acker.id.decode()))
+            commit_ack = add_as_commit(ack_act, repo, msg=ack_msg, deps=repo.show_ref("refs/heads/frontier/CHF/" + giver.id.decode()))
+            validate_hash(commit_give, "commit_give")
+            validate_hash(commit_ack, "commit_ack")
+            print(give_msg)
+            print(ack_msg)
+
+        run_cmd("git update-ref HEAD $(git log --format=%H -n 1 --all)", str(self._repo_dir))
+
+        for account in ledger.values():
+            print(f"{account!r}")
+    
+
 def get_public_keys(keydir: Path, names):
     ids: list[str] = []
     run_cmd(f"mkdir -p {keydir}") # in case the key directory doesn't exist
@@ -98,7 +172,7 @@ def configure_allowed_signers(repo_dir: Path, keydir: Path, author_names: list[s
 import argparse
 def generate_repo():
     parser = argparse.ArgumentParser(prog="generate", description="Generates a repository that contains an append-only log representing a GOC-Ledger")
-    parser.add_argument("type", help="the type of transactions to generate. possible values: valid_v1")
+    parser.add_argument("type", help="the type of transactions to generate. possible values: valid_v1, invalid_goc_given")
     parser.add_argument("-d", "--directory", help="directory of generated repository. Defaults to \"./db\"", default="./db")
     parser.add_argument("-c", "--no-commits", help="number of commits (log messages) generated. Defaults to 20", default=20)
     parser.add_argument("-u", "--no-users", help="number of users in transactions. Defaults to 4", default=4)
@@ -114,6 +188,8 @@ def generate_repo():
     match type:
         case "valid_v1":
             generator = ValidRepoGeneratorV1(db, no_commits, no_users, seed, sign)
+        case "invalid_goc_given":
+            generator = InvalidRepoGeneratorGoc(db, no_commits, no_users, seed, sign)
         case _:
             print(f"generator type {type} invalid!")
             exit(1)
