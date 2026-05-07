66f46b5f0e0524794cda4ef4d385ab33e8843878
pvonf <pvonf@shoreham>
2026-05-07 22:40:21 +0200

    verifier: add some testcase ideas, non functioning tho

--- code/src/verify_goc_ledger/tests/gen_testcases.py
+++ code/src/verify_goc_ledger/tests/gen_testcases.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from common.git_utils import Repo, add_delta_state_as_commit_plumbing
+from common.git_utils import Repo, add_delta_state_as_commit_plumbing, add_fork_proof_as_commit
 from common.misc import generate_human_names, write_verification_output_expected
 import sys
 import shutil
@@ -311,6 +311,58 @@ def generate_testcase14():
     invalid_commits = [a2, b2]
 
     write_verification_output_expected(test_dir, list(map(lambda x: x.encode(), valid_commits)), list(map(lambda x: x.encode(), invalid_commits)))
+
+def generate_testcase_fork_valid():
+    test_dir = Path("./testcases/fork_valid")
+    if os.path.exists(test_dir):
+        print(f"directory {test_dir} exists already, not generating.")
+        return
+    
+    a, b, c = generate_human_names(2)
+    repo = Repo(str(test_dir/"db"))
+    repo.create_repo()
+    date = 1774010000
+    a1 = add_delta_state_as_commit_plumbing(repo, [], a, date, created=100)
+    b1 = add_delta_state_as_commit_plumbing(repo, [], b, date, created=100)
+    c1 = add_delta_state_as_commit_plumbing(repo, [], c, date, created=100)
+    b2 = add_delta_state_as_commit_plumbing(repo, [b1, c1], b, date, given={c.encode(): 100})
+    b3 = add_delta_state_as_commit_plumbing(repo, [b1, a1], b, date, given={a.encode(): 100})
+    a2 = add_delta_state_as_commit_plumbing(repo, [a1, b3], a, date, acked={b.encode(): 100})
+    a3 = add_fork_proof_as_commit(repo, [b2, b3], a, b, date)
+
+    valid_commits = [a1, b1, c1, b2, b3, a2, a3]
+    invalid_commits = []
+
+    # TODO how exactly can I store the expected output?
+
+    write_verification_output_expected(test_dir, list(map(lambda x: x.encode(), valid_commits)), list(map(lambda x: x.encode(), invalid_commits)))
+
+def generate_testcase_fork_invalid():
+    test_dir = Path("./testcases/fork_invalid")
+    if os.path.exists(test_dir):
+        print(f"directory {test_dir} exists already, not generating.")
+        return
+    
+    a, b, c = generate_human_names(2)
+    repo = Repo(str(test_dir/"db"))
+    repo.create_repo()
+    date = 1774010000
+    a1 = add_delta_state_as_commit_plumbing(repo, [], a, date, created=100)
+    b1 = add_delta_state_as_commit_plumbing(repo, [], b, date, created=100)
+    c1 = add_delta_state_as_commit_plumbing(repo, [], c, date, created=100)
+    b2 = add_delta_state_as_commit_plumbing(repo, [b1, c1], b, date, given={c.encode(): 100})
+    b3 = add_delta_state_as_commit_plumbing(repo, [b1, a1], b, date, given={a.encode(): 100})
+    c2 = add_delta_state_as_commit_plumbing(repo, [c1, b2], c, date, acked={b.encode(): 100})
+    a2 = add_delta_state_as_commit_plumbing(repo, [a1, b3], a, date, acked={b.encode(): 100})
+    a3 = add_delta_state_as_commit_plumbing(repo, [a2, c2], a, date, given={c.encode(): 100}) # since a knows about c2, it must also know about b2 and since it also knows about b3, it knows about the fork.
+
+    valid_commits = [a1, b1, c1, b2, b3, a2]
+    invalid_commits = [a3]
+
+    # TODO how exactly can I store the expected output?
+
+    write_verification_output_expected(test_dir, list(map(lambda x: x.encode(), valid_commits)), list(map(lambda x: x.encode(), invalid_commits)))
+
 
 if __name__ == "__main__":
     main()
