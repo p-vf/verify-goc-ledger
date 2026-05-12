3d506bb32efb14856f8e33bfbdee6a5d58275df4
pvonf <pvonf@shoreham>
2026-05-12 21:01:58 +0200

    benchmark: improve some benchmark code for flexibility

--- code/src/verify_goc_ledger/bench/run_benchmarks.py
+++ code/src/verify_goc_ledger/bench/run_benchmarks.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
-import glob
-import filecmp
 import sys
-import time
+import argparse
 
-from common.misc import bcolors
 from test_git_cli import verify_repo
 
 def main():
-    specified_tests = set(sys.argv[1:])
+    parser = argparse.ArgumentParser(prog="generate", description="Generates a repository that contains an append-only log representing a GOC-Ledger")
+    parser.add_argument("-p", "--stat-prefix", help="set prefix of profile output", default="")
+    parser.add_argument("benches", nargs="*", help="specify what benchmarks to run")
+    args = parser.parse_args()
+    stat_prefix = args.stat_prefix
+    specified_tests = args.benches
     run_all_tests = False
     if len(specified_tests) == 0:
         run_all_tests = True
@@ -29,7 +31,7 @@ def main():
                 continue
             print(f"running {e}")
             # Run testcase
-            verify_repo(str(test_dir_full / e), test_dir_full / (e + ".stats"), False)
+            verify_repo(str(test_dir_full / e), test_dir_full / (stat_prefix + e + ".stats"), False)
 
 if __name__ == "__main__":
     main()
