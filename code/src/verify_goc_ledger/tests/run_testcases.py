43bdc49c6fcf4cca1b9b15c3145fe7a3256568c5
pvonf <pvonf@shoreham>
2026-05-28 13:08:35 +0200

    tests: add regex matching to test runner

--- code/src/verify_goc_ledger/tests/run_testcases.py
+++ code/src/verify_goc_ledger/tests/run_testcases.py
@@ -3,24 +3,23 @@ from pathlib import Path
 import glob
 import filecmp
 import sys
+import re
 
 from common.misc import bcolors
 from test_git_cli import verify_repo
 
 def main():
     specified_tests = set(sys.argv[1:])
-    run_all_tests = False
     if len(specified_tests) == 0:
-        run_all_tests = True
+        test_regex = re.compile(".*") # if no tests specified, run all of them
+    else:
+        test_regex = re.compile("(" + ")|(".join(specified_tests) + ")")
     testcase_dir = Path("./testcases")
     no_passed_testcases = 0
     no_failed_testcases = 0
     for test_dir in os.listdir(testcase_dir):
-        if not run_all_tests:
-            if test_dir not in specified_tests:
-                continue
-            else:
-                specified_tests.remove(test_dir)
+        if not test_regex.match(test_dir):
+            continue
         test_dir_full = testcase_dir / test_dir
         print(test_dir_full)
         assert os.path.isdir(test_dir_full)
@@ -50,7 +49,7 @@ def main():
                 # TODO diff the files here
                 if filecmp.cmp(file, e, False):
                     # yay!
-                    print("files equal, yay!")
+                    print(bcolors.OKGREEN + "files equal, yay!" + bcolors.ENDC)
                 else:
                     # nay :(
                     print(bcolors.FAIL + "files differ" + bcolors.ENDC)
@@ -64,8 +63,6 @@ def main():
         else:
             no_failed_testcases += 1
     print("==== TEST RESULTS ====")
-    if len(specified_tests) > 0:
-        print(bcolors.WARNING + f"WARNING: the specified tests {specified_tests} weren't found" + bcolors.ENDC)
     print(f"passed: {no_passed_testcases}, failed: {no_failed_testcases}")
     if no_failed_testcases == 0:
         print(bcolors.OKGREEN + "all tests passed, hurray!" + bcolors.ENDC)
