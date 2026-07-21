a03fcbf395c1ec4430696e013f8481902479a45d
pvonf <pvonf@shoreham>
2026-07-21 17:22:19 +0200

    misc changes in report

--- code/src/verify_goc_ledger/bench/generate_benchmarks.py
+++ code/src/verify_goc_ledger/bench/generate_benchmarks.py
@@ -1,17 +1,9 @@
-from main import ValidRepoGeneratorV1, ValidRepoGeneratorPareto, ValidRepoGeneratorParetoAckDelayed
+from main import ValidRepoGeneratorParetoAckDelayed
 from pathlib import Path
 
 def main():
-    valid_v1_path = Path(f"./benchmarks/valid_v1/")
-    valid_pareto_path = Path(f"./benchmarks/valid_pareto/")
     valid_pareto_ack_delayed_path = Path(f"./benchmarks/valid_pareto_ack_delayed/")
     for num_commits in range(300, 901, 300):
-        # g = ValidRepoGeneratorV1(valid_v1_path/f"db{num_commits:03}", num_commits, 10)
-        # if not g.generate():
-        #     print("failed to generate valid_v1")
-        # g = ValidRepoGeneratorPareto(valid_pareto_path/f"db{num_commits:03}", num_commits, 10, k=5)
-        # if not g.generate():
-        #     print("failed to generate valid_pareto")
         g = ValidRepoGeneratorParetoAckDelayed(valid_pareto_ack_delayed_path/f"{num_commits:03}", num_commits, 10, k=5, ack_delay=20)
         if not g.generate():
             print("failed to generate valid_pareto_ack_delayed")
